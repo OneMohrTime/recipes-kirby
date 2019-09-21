f91b4893ffa201177fbacfe2aea83d37eceb8639
Derek Mohr <derek@mightyinthemidwest.com>
2019-09-21 10:35:02 -0400

    [refs #11] Add component

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,43 +1,50 @@
-import '../../node_modules/tingle.js/src/tingle.js';
+var searchToggle = document.getElementById('searchToggle');
+console.log(searchToggle)
 
-// instanciate new modal
-var modal = new tingle.modal({
-    footer: true,
-    stickyFooter: false,
-    closeMethods: ['overlay', 'button', 'escape'],
-    closeLabel: "Close",
-    cssClass: ['custom-class-1', 'custom-class-2'],
-    onOpen: function() {
-        console.log('modal open');
-    },
-    onClose: function() {
-        console.log('modal closed');
-    },
-    beforeClose: function() {
-        // here's goes some logic
-        // e.g. save content before closing the modal
-        return true; // close the modal
-        return false; // nothing happens
-    }
-});
-
-// set content
-modal.setContent('<h1>here\'s some content</h1>');
-
-// add a button
-modal.addFooterBtn('Button label', 'tingle-btn tingle-btn--primary', function() {
-    // here goes some logic
-    modal.close();
-});
-
-// add another button
-modal.addFooterBtn('Dangerous action !', 'tingle-btn tingle-btn--danger', function() {
-    // here goes some logic
-    modal.close();
-});
-
-// open modal
-modal.open();
-
-// close modal
-modal.close();
+searchToggle.addEventListener('click', function(e) {
+	console.log(e.target)
+})
+//
+//import '../../node_modules/tingle.js/src/tingle.js';
+//
+//// instanciate new modal
+//var modal = new tingle.modal({
+//    footer: true,
+//    stickyFooter: false,
+//    closeMethods: ['overlay', 'button', 'escape'],
+//    closeLabel: "Close",
+//    cssClass: ['custom-class-1', 'custom-class-2'],
+//    onOpen: function() {
+//        console.log('modal open');
+//    },
+//    onClose: function() {
+//        console.log('modal closed');
+//    },
+//    beforeClose: function() {
+//        // here's goes some logic
+//        // e.g. save content before closing the modal
+//        return true; // close the modal
+//        return false; // nothing happens
+//    }
+//});
+//
+//// set content
+//modal.setContent('<h1>here\'s some content</h1>');
+//
+//// add a button
+//modal.addFooterBtn('Button label', 'tingle-btn tingle-btn--primary', function() {
+//    // here goes some logic
+//    modal.close();
+//});
+//
+//// add another button
+//modal.addFooterBtn('Dangerous action !', 'tingle-btn tingle-btn--danger', function() {
+//    // here goes some logic
+//    modal.close();
+//});
+//
+//// open modal
+//modal.open();
+//
+//// close modal
+//modal.close();
